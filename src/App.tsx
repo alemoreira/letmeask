39a78eb959a13d3bad8aadf94aad6285d26812d7
Alessandro Moreira <alessandro.moreira@gmail.com>
2021-08-31 19:59:16 -0300

    Component only

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route path="/" exact component={Home}/>
-      <Route path="/rooms/new" component={NewRoom}/>
-    </BrowserRouter>
+    <Home/>
   );
 }
 
