827687fe4ce0b939ecd095b33c93eab739400da0
Alessandro Moreira <alessandro.moreira@gmail.com>
2021-08-31 22:31:22 -0300

    BrowserRouter with basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import {HashRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import { Home } from "./pages/Home";
 import { NewRoom } from './pages/NewRoom';
 // import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/" exact component={Home}/>
         <Route path="/rooms/new" component={NewRoom}/>
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
